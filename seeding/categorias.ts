21566ba2ef1e1f823418187f80e6a46153f781b1
Isaac Fernández <ifernandeznv@gmail.com>
2023-02-26 17:13:09 -0600

    Primeros pasos en el seeding a la base de datos

---
+++ seeding/categorias.ts
@@ -0,0 +1,34 @@
+interface Categoria {
+    icono: string,
+    nombre: string
+}
+const categorias: Categoria[] = [
+    {
+        icono: "cafe",
+        nombre: "Café"
+      },
+      {
+        icono: "hamburguesa",
+        nombre: "Hamburguesas"
+      },
+      {
+        icono: "pizza",
+        nombre: "Pizzas"
+      },
+      {
+        icono: "dona",
+        nombre: "Donas"
+      },
+      {
+        icono: "pastel",
+        nombre: "Pasteles"
+      },
+      {
+        icono: "galletas",
+        nombre: "Galletas"
+      }
+]
+
+export {
+    categorias 
+}
