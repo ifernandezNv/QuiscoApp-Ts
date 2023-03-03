86aca317a06164824bb53c7b92c91abf66bd008f
Isaac Fernández <ifernandeznv@gmail.com>
2023-03-02 18:09:36 -0600

    Resaltando la categoria seleccionada

--- src/components/Categoria.tsx
+++ src/components/Categoria.tsx
@@ -7,19 +7,20 @@ import { useRouter } from 'next/router'
 function Categoria({categoria} : Categoria) {
     const {id, nombre, icono, productos} = categoria
     const router = useRouter();
-
+    
   return (
-    <div className={router.pathname === `/${icono}` ? 'bg-yellow-500 p-2 rounded w-full' :  'w-full p-2 rounded flex gap-4 items-center justify-center hover:bg-yellow-500 transition-all'} >
+    <Link href={`/${icono}`} className={router.asPath === `/${icono}` ? 'bg-yellow-500 p-2 rounded w-full flex gap-4 items-center justify-center my-2' :  'w-full p-2 rounded flex gap-4 items-center justify-center hover:bg-yellow-500 transition-all my-2'} >
       <Image
         alt={`Imagen de la categoria ${nombre}`}
         width={50}
         height={100}
         src={`/assets/img/icono_${icono}.svg`}
       />
-      <Link href={`/${icono}`} className='font-semibold text-center w-1/2'>
-          {nombre}
-      </Link>
-    </div>
+      <p className='font-semibold text-center w-1/2'>
+        {nombre}
+      </p>
+        
+    </Link>
     
   )
 }
