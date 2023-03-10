d3afa5d95beb89e674985d1fbd0f74b94a108c92
Isaac Fernández <ifernandeznv@gmail.com>
2023-03-10 16:44:28 -0600

    Primeros pasos en la página de resumen

---
+++ src/components/ProductoState.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+
+function ProductoState({producto}) {
+    const {nombre, precio, cantidad, id, imagen} = producto
+  return (
+    <div className='flex gap-5'>
+        <Image
+            width={200}
+            height={200}
+            alt={`Imagen del producto ${nombre}`}
+            src={`/assets/img/${imagen}.jpg`}
+        />
+        <div className=''>
+
+        </div>
+        <div>
+            <button type='button' className='bg-amber-500 p-2 my-3 text-center text-white font-semibold rounded'>Editar</button>
+            <button type='button' className='bg-red-600 p-2 my-3 text-center text-white font-semibold rounded'>Editar</button>
+        </div>
+    </div>
+  )
+}
+
+export default ProductoState
--- src/pages/resumen.tsx
+++ src/pages/resumen.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import useQuiosco from 'hooks/useQuiosco'
 import Layout from '@/components/Layout'
 function resumen() {
-  
+  const {orden} = useQuiosco()
   return (
     <Layout
         title={'Resumen'}
-        description='Visualiza el progreso del pedido que haz realizado '
+        description='Visualiza el progreso del pedido que haz realizado'
     >
         <h1 className='font-black text-4xl my-4'>Resumen</h1>
-        <p>Revisa tu pedido</p>
+        <p>{orden?.pedido.length ? 'Revisa, administra y edita los productos agregados a tu pedido' : 'No haz agregado productos a tu orden, comienza agregandolos'}</p>
+        <div className='flex flex-col gap-4'>
+          {orden?.pedido?.map(productoState => <ProductoCarrito key={productoState.id} producto={productoState}/> )}
+        </div>
     </Layout>
   )
 }
