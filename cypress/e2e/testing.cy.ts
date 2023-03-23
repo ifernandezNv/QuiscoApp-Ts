0aadf0673d4eb10104be408e858726663447ddc7
Isaac Fernández <ifernandeznv@gmail.com>
2023-03-23 17:28:23 -0600

    Estos test deberían de pasar sin problemas, pero no es así

--- cypress/e2e/testing.cy.ts
+++ cypress/e2e/testing.cy.ts
@@ -9,25 +9,25 @@ describe('Test visitando la página, verificando el contenido del sidebar y la c
   it('Filtrando los productos de la categoría Hamburguesas', ()=>{
     cy.visit('/')
     
-    // cy.get('a[data-cy=Hamburguesas]').as('btn').click()
-    cy.get('[data-cy=Hamburguesas]').click()
-    // cy.get('@btn').should('have.class', 'bg-yellow-500')
-    // cy.url().should('include', '/hamburguesa')
-    cy.get('h1[data-cy=headingCategoria]').should('have.text', 'Hamburguesas')
-    cy.get('h3[data-cy=Hamburguesas]').should('contain.text', 'Hamburguesa Sencilla')
-    cy.get('p[data-cy=precio]').should('contain.text', '$59.90')
-  })
-  it('Verificando la funcionalidad y el correcto redireccionamiento del progress bar', ()=>{
-    cy.visit('/')
-    cy.get('a[data-cy=resumen]').click()
-    cy.url().should('include', '/resumen')
-    cy.get('div[data-cy=progreso]').should('have.class', 'w-2/3')
-    cy.get('h1[data-cy=headingResumen]').should('have.text', 'Resumen de tu Pedido')
-    cy.get('a[data-cy=datos]').click()
-    cy.url().should('include', '/datos')
-    cy.get('div[data-cy=progreso]').should('have.class', 'w-full')
-    cy.get('h1[data-cy=headingDatos]').should('have.text', 'Total y Confirmar Pedido')
-  })
+  // cy.get('a[data-cy=Hamburguesas]').as('btn').click()
+  cy.get('[data-cy=Hamburguesas]').click()
+  // cy.get('@btn').should('have.class', 'bg-yellow-500')
+  // cy.url().should('include', '/hamburguesa')
+     cy.get('h1[data-cy=headingCategoria]').should('have.text', 'Hamburguesas')
+     cy.get('h3[data-cy=hamburguesa]').should('contain.text', 'Hamburguesa Sencilla')
+     cy.get('p[data-cy=precio]').should('contain.text', '$59.90')
+   })
+   it('Verificando la funcionalidad y el correcto redireccionamiento del progress bar', ()=>{
+     cy.visit('/')
+     cy.get('a[data-cy=resumen]').click()
+     cy.url().should('include', '/resumen')
+     cy.get('div[data-cy=progreso]').should('have.class', 'w-2/3')
+     cy.get('h1[data-cy=headingResumen]').should('have.text', 'Resumen de tu Pedido')
+     cy.get('a[data-cy=datos]').click()
+     cy.url().should('include', '/datos')
+     cy.get('div[data-cy=progreso]').should('have.class', 'w-full')
+     cy.get('h1[data-cy=headingDatos]').should('have.text', 'Total y Confirmar Pedido')
+   })
   it('Escribiendo el nombre del cliente, sin llenar el pedido, y verificando el contenido de la alerta', ()=>{
     cy.visit('/')
     cy.get('a[data-cy=datos]').click()
@@ -50,18 +50,18 @@ describe('Test visitando la página, verificando el contenido del sidebar y la c
     cy.get('button[data-cy=aumentar]').click()
     cy.get('button[data-cy=agregarAlPedido]').click()
     cy.get('button[data-cy=cerrarModal]').click()
-    //Agregando un segundo producto
+    //Agregando modificando la cantidad del producto y verificando cambio al texto del boton para agregar al pedido
     cy.get('button[data-cy=botonAgregar]').first().click()
 
-    cy.get('button[data-cy=aumentar]').dblclick()
+    cy.get('button[data-cy=aumentar]').click()
+    cy.get('button[data-cy=aumentar]').click()
     cy.get('button[data-cy=agregarAlPedido]').should('have.text', 'Guardar Cambios')
-    
+
     cy.get('button[data-cy=agregarAlPedido]').click()
     cy.get('button[data-cy=cerrarModal]').click()
     //Viendo el resumen del pedido y confirmándolo
     cy.get('a[data-cy=resumen]').click()
     cy.get('button[data-cy=confirmarPedido]').click()
-
     cy.url().should('include', '/datos')
     cy.get('input[data-cy=nombre]').type('Isaac Fernández Navarro')
     cy.get('button[data-cy=confirmar]').click()
